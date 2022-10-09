7e989f999b0128651c92b651f683a33233cfb0b2
sunj__@naver.com <sunj__@naver.com>
2022-10-09 23:54:27 +0900

    setting

---
+++ src/main/java/com/example/CLOOK/ClookApplication.java
@@ -0,0 +1,13 @@
+package com.example.CLOOK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClookApplication.class, args);
+	}
+
+}
