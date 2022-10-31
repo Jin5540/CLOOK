46e1ff05146570ddd4820915c724ebd0e951dfb2
sunj__@naver.com <sunj__@naver.com>
2022-10-31 19:59:59 +0900

    다시 변경

--- src/main/java/com/example/CLOOK/ClookApplication.java
+++ src/main/java/com/example/CLOOK/ClookApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ClookApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClookApplication.class, args);
 	}
