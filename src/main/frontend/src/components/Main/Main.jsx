b2b9cddc8f9dd1eb6da71599c3230ac4236882bd
jade0819 <break0819@gmail.com>
2022-12-06 01:56:59 +0900

    JSX: Main.jsx - Card 문구 다시 수정 완료.

--- src/main/frontend/src/components/Main/Main.jsx
+++ src/main/frontend/src/components/Main/Main.jsx
@@ -3,6 +3,7 @@ import { faBell } from "@fortawesome/free-solid-svg-icons";
 import Card from "../Shared/Card/Card";
 import Icon from "../Shared/Icon/Icon";
 import CurrentWeather from "./CurrentWeather";
+import { timeFormat } from "../../util/timeFormat";
 
 // 메인 카드 컴포넌트로 만들기 (재사용 - 날씨에 따라 배경이랑 텍스트만 바꿔주면됨)
 
@@ -10,23 +11,28 @@ export default function MainWeather({ toptm, topspt }) {
   console.log(toptm.time);
   console.log(toptm.message);
 
+  // const time = ["0000", "0600", "0800"];
+  // const message = ["눈", "비", "우박"];
+
   return (
     <section className="w-full bg-white mb-12">
       {toptm && topspt && (
         <>
           <CurrentWeather toptm={toptm} topspt={topspt} />
-          <Card styles="relative">
-            <div className="absolute top-1/2 left-6 -translate-y-2/4">
-              <Icon icon={faBell} size={26} />
-            </div>
-            <span className="text-2xl text-brand font-medium">
-              {toptm.hasOwnProperty("time") && toptm.hasOwnProperty("time")
-                ? `${toptm.time[0] === "0000" ? "" : toptm.time[0]} ${
-                    toptm.message[0]
-                  } 소식이 있어요.`
-                : "ex 5시간 뒤에 비 소식이 있어요."}
-            </span>
-          </Card>
+          {toptm.hasOwnProperty("time") && toptm.hasOwnProperty("time") && (
+            <Card styles="relative">
+              <div className="absolute top-1/2 left-6 -translate-y-2/4">
+                <Icon icon={faBell} size={26} />
+              </div>
+              <span className="text-2xl text-brand font-medium">
+                {toptm.time.map((item, index) => {
+                  return `${timeFormat(item)} ${toptm.message[index]}${
+                    toptm.time.length - 1 === index ? " 소식이 있어요." : ", "
+                  }`;
+                })}
+              </span>
+            </Card>
+          )}
         </>
       )}
     </section>
