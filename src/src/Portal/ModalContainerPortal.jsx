c4217655276a5432fe7c9d0a844f082bfb969f57
yesjin <sunj__@naver.com>
2022-12-14 09:30:41 +0900

    변경

--- src/src/Portal/ModalContainerPortal.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createPortal } from "react-dom";
-
-export default function ModalContainerPortal({ children }) {
-  return createPortal(<>{children}</>, document.querySelector("#modal-root"));
-}
