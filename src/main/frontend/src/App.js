e5b571a2c0ee746bef2b3b1d98a10674e50c996a
jade0819 <break0819@gmail.com>
2023-01-11 05:20:13 +0900

    CSS: 검색 버튼 삭제, 섹션 줄 맞춤, logo 변경, 메인 섹션의 날씨 1문장 내용 두 줄 이상일 때의 너비 조절, 시간대별 옷차림의 카드안에 시간 width 수정, 모달 그림자 추가, 모달 닫기 버튼 위치 조정 / JSX:  차트 기온 글자색 추가, 강수량 mm 추가, 서비스 점수 및 의견에 점수 0점 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { useQueryErrorResetBoundary } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
-import Error from "./components/Shared/Error/Error";
+import GlobalError from "./components/Shared/Error/GlobalError";
 import Header from "./components/Header/Header";
 import Content from "./components/Shared/Content/Content";
 import Footer from "./components/Footer/Footer";
 import Survey from "./components/Shared/Survey/Survey";
 
 export default function App() {
+  const { reset } = useQueryErrorResetBoundary();
+
   return (
-    <ErrorBoundary FallbackComponent={Error}>
+    <ErrorBoundary onReset={reset} FallbackComponent={GlobalError}>
       <div className="relative flex flex-col items-center w-full min-h-full bg-white">
         <Header />
         <Content />
